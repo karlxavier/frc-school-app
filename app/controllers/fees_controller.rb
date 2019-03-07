9d8cf90f9fd003441c02ea9d2e9b08cfea4f1498
karlxavier <kxdc1025@yahoo.com>
2019-03-07 12:11:57 +0400

    no need to call api just call local DB

--- app/controllers/fees_controller.rb
+++ app/controllers/fees_controller.rb
@@ -3,26 +3,47 @@ class FeesController < ApplicationController
 
   def index
     @student = ''
+    @student_api = ""
     if params[:filter].present?
-      url = "http://sunrise.fortiddns.com:8088/sunrise/orisondata.ashx?head=FullStudent"
-      @results = HTTParty.get(url)
-      @student = (@results['Student'].find { |u| u['Code'] == params[:filter][:code] })
-      
+      @student = Student.where(code: params[:filter][:code]).first
       if @student.present?
-        if Fee.where(student_id: @student['Code']).present?
-          @fees = Fee.where(student_id: @student['Code']).first
+        if Fee.where(student_id: @student.code).present?
+          @fees = Fee.where(student_id: @student.code).first
 
-          session[:ParentName] = @student['ParentName']
-          session[:FatherMobile] = @student['FatherMobile']
-          session[:BusArea] = @student['BusArea']
-          session[:FatherEmail] = @student['FatherEmail']
+          session[:ParentName] = @student.parent_name
+          session[:FatherMobile] = @student.father_mobile
+          session[:BusArea] = @student.address1
+          session[:FatherEmail] = @student.father_email
 
-          session[:StudentName] = @student['Name']
-          session[:StudentCode] = @student['Code']
-          session[:StudentClass] = @student['Class']
-          session[:StudentDivision] = @student['Division']
+          session[:StudentName] = @student.name
+          session[:StudentCode] = @student.code
+          session[:StudentClass] = @student.student_class
+          session[:StudentDivision] = @student.division
         else
           @fees = nil
+        end
+      else
+        url = "http://sunrise.fortiddns.com:8088/sunrise/orisondata.ashx?head=FullStudent"
+        @results = HTTParty.get(url)
+        @student_api = (@results['Student'].find { |u| u['Code'] == params[:filter][:code] })
+        
+        if @student_api.present?
+          puts '**************** not present'
+          if Fee.where(student_id: @student_api['Code']).present?
+            @fees = Fee.where(student_id: @student_api['Code']).first
+
+            session[:ParentName] = @student_api['ParentName']
+            session[:FatherMobile] = @student_api['FatherMobile']
+            session[:BusArea] = @student_api['BusArea']
+            session[:FatherEmail] = @student_api['FatherEmail']
+
+            session[:StudentName] = @student_api['Name']
+            session[:StudentCode] = @student_api['Code']
+            session[:StudentClass] = @student_api['Class']
+            session[:StudentDivision] = @student_api['Division']
+          else
+            @fees = nil
+          end
         end
       end
     end
